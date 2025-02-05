70c5a5344b7f36fe430f79514c9770d4998bae2d
Mohammad Talim <talimmohammad116@gmail.com>
2025-02-05 23:04:28 +0530

    feat: add support for scanning the division operator & comments

--- cmd/myinterpreter/scanner.go
+++ cmd/myinterpreter/scanner.go
@@ -9,10 +9,13 @@ func tokenize(fileContents []byte) ([]Token, bool) {
 	tokens := []Token{}
 	lineNumber := 1
 	lexicalError := false
+	isComment := false
 	length := len(fileContents)
 
 	for i := 0; i < length; i++ {
 		char := fileContents[i]
+		isComment = false
+
 		switch char {
 		case '(':
 			tokens = append(tokens, Token{LEFT_PAREN, "(", nil})
@@ -64,9 +67,19 @@ func tokenize(fileContents []byte) ([]Token, bool) {
 			} else {
 				tokens = append(tokens, Token{GREATER, ">", nil})
 			}
+		case '/':
+			if i+1 < length && fileContents[i+1] == '/' {
+				isComment = true
+			} else {
+				tokens = append(tokens, Token{SLASH, "/", nil})
+			}
 		default:
 			fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %c\n", lineNumber, char)
 			lexicalError = true
+		}
+
+		if isComment {
+			break
 		}
 	}
 
